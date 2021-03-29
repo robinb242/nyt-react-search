25b133ebdc8650e1a28ff53f58ae2b3e5cd6eeec
robinb242 <robin.bertuccelli@gmail.com>
2021-03-28 20:46:43 -0500

    so close

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ const bluebird = require("bluebird");
 //Set up a default port
 const PORT = process.env.PORT || 3001;
 mongoose.Promise = bluebird;
-const app = express();
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
@@ -44,8 +43,7 @@ app.use(router);
 
  
 // Connect mongoose to our database
-const db = process.env.MONGODB_URI || "mongodb://localhost/nyt-react", { useNewUrlParser: true };
-mongoose.connect(db, function(error) {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/nyt-react", { useNewUrlParser: true }, { useUnifiedTopology: true }, function(error) {
   // Log any errors connecting with mongoose
   if (error) {
     console.error(error);
